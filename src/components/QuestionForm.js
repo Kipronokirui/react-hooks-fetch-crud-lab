646636db2e5f909797953cda6448cf37680ed741
Kipronokirui <victorkipronokirui2@gmail.com>
2024-02-08 19:11:57 +0300

    Assignment Complete

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -20,6 +20,16 @@ function QuestionForm(props) {
   function handleSubmit(event) {
     event.preventDefault();
     console.log(formData);
+    fetch('http://localhost:3000/questions', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
   }
 
   return (
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questionsList, setQuestionsList] = useState([])
+  useEffect(() => {
+    console.log('UseEffect works')
+    fetch('http://localhost:3000/questions')
+    .then(response => response.json())
+    .then(data => setQuestionsList(data))
+  }, [])
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {/* display QuestionItem components here after fetching */}
+        {questionsList?.map((question, index) => (
+          <QuestionItem key={question.id} question={question} />
+        ))}
+      </ul>
     </section>
   );
 }
